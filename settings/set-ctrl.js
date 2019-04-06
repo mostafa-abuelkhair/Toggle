7bb5f3193c25eae5d02ca1f09954c93a974cb69e
mostafa <mostafa.ahmed2509@gmail.com>
2019-04-06 15:26:25 +0200

    edits to setting page

--- settings/set-ctrl.js
+++ settings/set-ctrl.js
@@ -4,6 +4,9 @@ $rootScope.back=[$rootScope.back[0]];
   $scope.inc=function(x) {
   $scope.bckf('settings/setpage.htm');
   $rootScope.sp=x;
+  $scope.sname=x.name;
+  $scope.skey=x.key;
+  $scope.appass=x.appass;
   }
 
 });
