0ac5cc93285f82defdcc9500e41444466bd23d3e
mostafa <mostafa.ahmed2509@gmail.com>
2019-06-09 18:46:50 +0200

    add ls/lg to tree

--- tree/tree-ctrl.js
+++ tree/tree-ctrl.js
@@ -1,49 +1,34 @@
 app.controller('tree-ctrl', function($scope,$rootScope) {
 
-/*$scope.d={
-           s:[{
-                t:{name:'home',id:"n",out:'roots'},
-                s:[
-                   {t:{name:'room1',id:"12d34f5hjkli",out:'ch1'},s:[]},
-                   {t:{name:'room2',id:"",out:'roots'},s:[]},
-                   {
-                     t:{name:'room3',id:"",out:'roots'},
-                     s:[
-                         {t:{name:'out1',id:"",out:'roots'},s:[]},
-                         {t:{name:'out2',id:"",out:'roots'},s:[]}
-                       ]
-                   }
-                  ]
-              }]
-         };*/
 
-         $scope.d={
+         var it={
                     s:[{
                          t:{name:'home',id:"",out:'roots'},
                          s:[]
                        }]
                   };
 
+          var tree = $rootScope.lsg('tree')
+          $scope.d= tree==undefined? it : JSON.parse(tree);
+
 
 $scope.node=function(oba,idx){
-
 
 $('#myModal').modal('toggle');
 $scope.t=oba[idx].t;
 $scope.oba=oba;
 $scope.idx=idx;
-}
 
-/*$scope.products=[
-  {id:'12d34f5hjkli',pid:'control unit0.2',name:'control unit',outputs:{ch1:1,ch2:0,ch3:1,ch4:0,ch5:0}}
-  ,{id:'asd2',pid:'control unit0.2',name:'control_unit',outputs:{ch1:0,ch2:1,ch3:1,ch4:0,ch5:1}}
-];*/
+}
 
 $scope.p=function(ob){
 return $rootScope.products.find((e)=>e.id==ob.t.id);
 }
 
-$scope.mc=function(){$('#myModal').modal('toggle');}
+$scope.mc=function(){
+  $('#myModal').modal('toggle');
+  $rootScope.lss('tree',JSON.stringify($scope.d));
+}
 
 $scope.req=function(d){
 if($scope.con(d)){
