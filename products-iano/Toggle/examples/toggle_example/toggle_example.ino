8bb0a11906c09d0e75f776d061f439149b502e46
mostafa <mostafa.ahmed2509@gmail.com>
2019-06-14 00:51:27 +0200

    sm-edits

--- products-iano/Toggle/examples/toggle_example/toggle_example.ino
+++ products-iano/Toggle/examples/toggle_example/toggle_example.ino
@@ -63,14 +63,14 @@ String data()   //function to send data to app
     
  String outputs_k[]={};      //outputs keys
  int outputs_v[]={};         //outputs values
- int outputs_n=5;            //outputs number
+ int outputs_n=0;            //outputs number
  String sensors_k[]={};      //sensors keys 
  String sensors_v[]={};      //sensors values
- int sensors_n;              //sensors number
+ int sensors_n=0;              //sensors number
  String data_k[]={};         //data keys
  String data_v[]={};         //data values
  int data_t[]={};            //data type   1 for string  0 for others
- int data_n=2;               //data number
+ int data_n=0;               //data number
   
   return d_add(data_k,data_v,data_t,data_n,outputs_k,outputs_v,outputs_n,sensors_k,sensors_v,sensors_n);
   }
